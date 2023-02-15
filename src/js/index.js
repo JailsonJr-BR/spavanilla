3a767e2c529a55171be6c6f477ae25f85b1052c6
Jailson <kronozrn@gmail.com>
2023-02-15 03:07:06 -0300

    refactor: move active navigation method to a function in utils

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 //import { renderNavLinks } from "./routes/render-nav-links.js";
 import { handleRoute } from "./router.js";
+import { activeNav } from "./utils.js";
 
 const router = new handleRoute()
 router.add("/", "../src/pages/home.html")
@@ -9,18 +10,7 @@ router.add(404, "../src/pages/404.html")
 router.handle()
 router.renderNavLinks()
 
-const navList = document.querySelector("#navLinksRender");
-const navItems = navList.getElementsByClassName("nav-item");
-
-for (const navItem of navItems) {
-  navItem.addEventListener("click", () => {
-    for (const item of navItems) {
-      item.classList.remove("active")
-    }
-    navItem.classList.add("active")
-  })
-}
-
+activeNav()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
---
+++ src/js/utils.js
@@ -0,0 +1,14 @@
+export function activeNav () {
+  const navList = document.querySelector("#navLinksRender");
+  const navItems = navList.getElementsByClassName("nav-item");
+
+  for (const navItem of navItems) {
+    navItems[0].classList.add("active")
+    navItem.addEventListener("click", () => {
+      for (const item of navItems) {
+        item.classList.remove("active")
+      }
+      navItem.classList.add("active")
+    })
+  }
+}
